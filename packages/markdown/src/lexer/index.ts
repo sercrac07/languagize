e8b7e9e9a1d48d05dbbbec685a1675b6e4e5aee8
sercrac07 <sercassan@gmail.com>
2025-01-04 01:02:30 +0100

    (markdown) add basic tokens

--- packages/markdown/src/lexer/index.ts
+++ packages/markdown/src/lexer/index.ts
@@ -1,13 +1,64 @@
-import { Token } from './types'
+import { Token, TokenType } from './types'
 
 class Lexer {
-  private chars: string[] = []
+  private source = ''
+  private position = { row: 0, column: 0 }
+  private cursor = 0
+
+  private Spec: [RegExp, (typeof TokenType)[keyof typeof TokenType]][] = [
+    [/^\s+/, TokenType.Whitespace],
+    [/^[^\s]+/, TokenType.Word],
+  ]
+
+  private getPosition(): typeof this.position {
+    return { ...this.position }
+  }
+  private hasRemaining(): boolean {
+    return this.cursor < this.source.length
+  }
 
   public tokenize(source: string): Token[] {
     const tokens: Token[] = []
-    this.chars = source.split('')
+    this.source = source
+
+    while (this.hasRemaining()) {
+      tokens.push(this.match())
+    }
+
+    tokens.push({ type: TokenType.EndOfFile })
 
     return tokens
+  }
+
+  private match(): Token {
+    for (const [regex, type] of this.Spec) {
+      const match = regex.exec(this.source.slice(this.cursor))
+
+      if (!match) continue
+
+      const value = match[0]
+      const start = this.getPosition()
+
+      this.cursor += value.length
+      this.position.column += value.length
+
+      if (value.includes('\n')) {
+        this.position.row++
+        this.position.column = 0
+      }
+
+      return {
+        type,
+        value,
+        range: {
+          start,
+          end: this.getPosition(),
+        },
+      }
+    }
+
+    const pos = this.getPosition()
+    throw new Error(`Unexpected token "${this.source[this.cursor]}" at ${pos.row}:${pos.column}`)
   }
 }
 
